f0c97d111beb09b1a2c842ff4a7c5e9a6b4dd787
LemperorD <ld2382619813@163.com>
2026-05-31 21:55:00 +0800

    initial commit

---
+++ code/README.md
@@ -0,0 +1,5 @@
+# code
+
+此处存放示例代码、作业等
+
+> Talk is cheap, show me the code.  --Linus Torvalds
---
+++ code/learning_mpc/learning_mpc.cpp
@@ -0,0 +1,10 @@
+﻿// learning_mpc.cpp: 定义应用程序的入口点。
+//
+
+#include "learning_mpc.h"
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
